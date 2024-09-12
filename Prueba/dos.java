ae04086efb31d93f41cb141c540b39181a998302
Lucia <luciferma14@gmail.com>
2024-09-12 19:45:11 +0200

    Update

---
+++ Prueba/dos.java
@@ -0,0 +1,13 @@
+
+
+public class dos {
+
+    public static void main (String[] args){
+
+        System.out.println("Adiós mundo");
+       
+
+    }
+    
+
+}
---
+++ Prueba/uno.java
@@ -0,0 +1,13 @@
+
+
+public class uno {
+
+    public static void main (String[] args){
+
+        System.out.println("Hola mundo");
+        System.out.println("Eres genial");
+
+    }
+    
+
+}
