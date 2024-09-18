c867553625f0b72e46da2794d62a46c05e285b48
Lucia <luciferma14@gmail.com>
2024-09-18 20:26:06 +0200

    Update

---
+++ EjerciciosAmpliados/Ampliado4.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Ampliado4 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        String nombre;
+
+        System.out.println("Cúal es tu nombre: ");
+        
+        nombre = sc.nextLine();
+
+        System.out.printf("Buenos dias, mi nombre es %s\n", nombre);
+
+
+
+
+
+    }
+
+
+
+    
+}
