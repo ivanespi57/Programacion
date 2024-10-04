2e3da49968ca468fd32431a197d90ed27e6a2402
Ivan <ivanespiasins75@gmail.com>
2024-10-04 13:07:33 +0200

    Update

---
+++ EjeAlgoritmos/algo5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class algo5 {
+    public static void main(String[] args) {
+        
+        int año;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Introduce un año: ");
+        año = sc.nextInt();
+
+        if (año == 0){
+            System.out.println("Fin del programa");
+        }else if (año )
+    }
+    
+}
