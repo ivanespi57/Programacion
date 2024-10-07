78ae4ecce5073d13422e1c9061008bfee356bd9b
Ivan Espi <ivanespiasins75@gmail.com>
2024-10-07 18:19:18 +0200

    Update

--- EjeAlgoritmos/algo4.java
+++ EjeAlgoritmos/algo4.java
@@ -7,18 +7,18 @@ public class algo4 {
         int n;
         int par = 0;
         int impar = 0;
-        int npar;
-        int nimpar;
+        double npar  ;
+        double nimpar ;
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Escribe la cantidad de números que quieres poner: ");
         cant = sc.nextInt();
 
+        System.out.println("Números: ");
         
-        
-        for (int i = 0; i <= cant; i++){
-            System.out.println("Números: ");
+        for (int i = 0; i < cant; i++){
+            
             n = sc.nextInt();
 
             if (n%2 == 0){
@@ -29,10 +29,12 @@ public class algo4 {
                 
             }
         }
-        npar = par / cant;
-        nimpar = impar / cant;
+        // calculo de los porcentajes fuera del FOR
         
-        System.out.printf("Porcentaje de pares: %d%\n Porcentaje de impares: %d%",npar, nimpar);
+        npar = (double) par / cant  * 100;
+        nimpar = (double) impar / cant * 100;
+        
+        System.out.printf("Porcentaje de pares: %.2f%%\nPorcentaje de impares: %.2f%%\n",npar, nimpar);
     }
     
 }
---
+++ EjeAlgoritmos/algo5.java
@@ -0,0 +1,7 @@
+public class algo5 {
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
