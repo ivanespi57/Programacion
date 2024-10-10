2de195db923af12118150122d14adcc3adf84ac6
Ivan Espi <ivanespiasins75@gmail.com>
2024-10-10 20:29:24 +0200

    Update

---
+++ EjerciciosPE/PE1.java
@@ -0,0 +1,13 @@
+public class PE1 {
+    public static void main(String[] args) {
+         
+        for (int i = 0;i <= 100;i++){
+            if (i%5 == 0) {
+                System.out.println(i);
+            }
+        }
+
+
+    }
+
+}
