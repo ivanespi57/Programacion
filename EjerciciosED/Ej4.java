81b8ebc7879b46b755990b0813afbb5f80f44334
Ivan <ivanespiasins75@gmail.com>
2025-01-02 15:51:09 +0100

    Update

---
+++ EjerciciosED/Ej4.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ej4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+
+        int sig = 0; 
+        int aleatorio;
+        int numeros; 
+
+        for (int i = 1; i <= 90; i++){
+            lista.add(i);
+        }
+
+        System.out.println("BINGO");
+        System.out.println("Estos son los números cantados: ");
+
+        while (lista.size() > 0){   
+            System.out.println("Pulsa 0 y luego Enter para mostrar el siguiente número: ");
+            sig = sc.nextInt();
+            
+            if (sig == 0){
+                aleatorio = (int) ((Math.random() * lista.size()));
+                numeros = lista.get(aleatorio);
+    
+                System.out.println("¡" + numeros + "!");
+    
+                lista.remove(aleatorio); // para borrar el número de la lista
+            }else{
+                System.out.println("Ese  no es el 0, intentalo otra vez!");
+            }
+        }
+        
+        
+    }
+}
