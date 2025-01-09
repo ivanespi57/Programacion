a253fd6ffdd195665dd5551fd5dafd434650a12a
Ivan Espi <ivanespiasins75@gmail.com>
2025-01-09 20:16:13 +0100

    Update

--- EjerciciosED/Ej3.java
+++ EjerciciosED/Ej3.java
@@ -3,11 +3,59 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Ej3 {
+
+    public static boolean esBalanceada(String s){
+
+        Stack<Character> pila = new Stack<Character>();
+
+        char carac;
+        char saca;
+        boolean balanceada = true;
+        
+        for (int i = 0; i < s.length() && balanceada; i++) {
+            carac = s.charAt(i);
+
+            if (carac == '(' || carac == '{' || carac == '['){
+                pila.push(carac);
+            }else{
+                if (carac == ')' || carac == '}' || carac == ']'){
+                    if (pila.empty()){
+                        balanceada = false;
+                    }else{
+                        saca = pila.pop();
+
+                    if (saca == '('&& carac != ')'){
+                        balanceada = false;
+                    }
+                    if (saca == '{'&& carac != '}'){
+                        balanceada = false;
+                    }
+                    if (saca == '['&& carac != ']'){
+                        balanceada = false;
+                    }
+                }
+                    
+            }
+        }
+    }
+                
+    return balanceada;
+
+    }
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
 
-        Stack<Character> pila = new Stack<Character>();
+        if (esBalanceada(sc.nextLine())){
+
+            System.out.println("SI");
+        }else{
+            System.out.println("NO");
+        }
+        
+        
+        
+        /* Stack<Character> pila = new Stack<Character>();
 
         String linea = sc.nextLine();
         char carac;
@@ -22,11 +70,12 @@ public class Ej3 {
                 
                 if (carac == ')' && saca == '('){
                     System.out.println("SI");
+                    
                 }
             }
         }
         
-
+        */
 
     }
 }
